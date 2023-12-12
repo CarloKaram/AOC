8ec381b1f3898278c2749d3dedc4e1e26b6f4185
CarloKaram <karam.carlo@hotmail.com>
2023-12-12 19:46:36 +0100

    day11 - very messy needs cleanup

---
+++ day_11/01.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+struct ind_pair {
+  int x;
+  int y;
+};
+
+std::vector<std::vector<char>> buildMap(std::string filename) {
+
+  std::vector<std::vector<char>> observation;
+  std::ifstream infile(filename);
+  char body;
+
+  for (std::string line; getline(infile, line);) {
+
+    std::vector<char> temp;
+
+    std::stringstream sequence(line);
+
+    while (sequence >> body) {
+      temp.push_back(body);
+    }
+
+    observation.push_back(temp);
+  }
+
+  return observation;
+}
+
+int main(int argc, char *argv[]) {
+
+  std::vector<std::vector<char>> image = buildMap(argv[1]);
+  std::vector<std::vector<char>> expandedImage;
+  int doubled = 0;
+  std::vector<int> cols;
+  std::vector<ind_pair> coords;
+  ind_pair index;
+
+  // Expand empty rows
+  for (int i = 0; i < image.size(); i++) {
+    expandedImage.push_back(image[i]);
+    if (std::none_of(image[i].begin(), image[i].end(),
+                     [](char c) { return c == '#'; })) {
+      expandedImage.push_back(image[i]);
+    }
+  }
+
+  for (int i = 0; i < image[0].size(); i++) {
+    std::vector<char> temp;
+    for (auto &seq : image) {
+      temp.push_back(seq[i]);
+    }
+    if (std::none_of(temp.begin(), temp.end(),
+                     [](char c) { return c == '#'; })) {
+      cols.push_back(i);
+      std::cout << i << std::endl;
+    }
+  }
+
+  for (int index : cols) {
+    for (int i = 0; i < expandedImage.size(); i++) {
+      expandedImage[i].insert(expandedImage[i].begin() + index + doubled, '.');
+    }
+    doubled += 1;
+  }
+
+  // Find coordinates of galaxies
+  for (int i = 0; i < expandedImage.size(); i++) {
+
+    for (int j = 0; j < expandedImage[0].size(); j++) {
+      if (expandedImage[i][j] == '#') {
+        index.x = j;
+        index.y = i;
+        coords.push_back(index);
+      }
+    }
+  }
+
+  long sum = 0;
+
+  while (coords.size() > 0) {
+
+    index = coords.back();
+    coords.pop_back();
+
+    for (auto &coord : coords) {
+      sum += abs(index.x - coord.x) + abs(index.y - coord.y);
+    }
+  }
+
+  std::cout << sum << std::endl;
+
+  return 0;
+}
